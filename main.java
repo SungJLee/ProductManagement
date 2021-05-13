c44da92d1659277a5a5a174d7ff8b7ff533b3d5c
SungJLee <songoku3212@gmail.com>
2021-05-13 14:04:45 +0900

    카페관리 프로그램 메인 수정2 (계산과 재고조사 추가)

--- main.java
+++ main.java
@@ -1,9 +1,48 @@
-	package cafe;
+	
 	
 	import java.util.Scanner;
 	import java.util.*;
 	
 	public class main {
+	
+		public void LookUpEatingInventory(String inventoryname, Object inventory) { // 6번
+			if(inventoryname.equals("cookie")) {
+				System.out.println("쿠키의 양"+ inventoryname.amount);
+			}else if(inventoryname.equals("cake")){
+				System.out.println("케이크의 양" + inventoryname.amount);
+			}
+			
+		}
+		
+		public double CalculateOrder(double sum) { // 3번
+			Scanner scanf = new Scanner(System.in);		
+			
+			System.out.println("총 가격은 :" + sum + "원 입니다.");
+			
+			
+			boolean swi = true;
+			int number;
+			number = scanf.nextInt();
+			
+			System.out.println("쿠폰을 가지고 계신가요?");
+			System.out.println("1. 네     2. 아니오");
+			
+			while(swi == true) {
+				switch(number){
+					case 1 :
+						sum = sum * 0.8;
+						swi = false;
+						System.out.println("20% 할인이 적용 되었습니다..");
+					case 2 :
+						swi = false;
+					default :
+						System.out.println("잘못 입력하셨습니다.");
+				}
+			}
+			
+			System.out.println(sum + "원 입니다.");
+			return sum;
+		}
 		
 		public static void main(String[] args) {
 		boolean swi = true;
@@ -28,6 +67,7 @@
 			switch(choice){
 			            
 				case 1:
+					
 					break;
 			      
 			    case 2:
@@ -45,7 +85,7 @@
 				case 6: 
 					break;
 			
-			    case 7 : // ������
+			    case 7 : 
 					swi = false;
 					break;
 				default :
@@ -55,3 +95,6 @@
 			}
 		}
 	}
+
+	
+	
